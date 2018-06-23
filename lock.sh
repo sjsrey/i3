aaee3ee4db219814fd6f0ab11edd8cf7f171829d
Serge Rey <sjsrey@gmail.com>
2018-06-23 10:15:27 -0700

    adding indicator for entering password

--- lock.sh
+++ lock.sh
@@ -2,6 +2,8 @@
 
 scrot /tmp/screen.png
 convert /tmp/screen.png -paint 3 /tmp/screen.png
+convert /tmp/screen.png -swirl -360 /tmp/screen.png
 [[ -f ~/.config/i3/lock.png ]] && convert /tmp/screen.png  ~/.config/i3/lock.png -gravity center -composite -matte /tmp/screen.png
 mocp -P
-i3lock -u -e -i /tmp/screen.png
+#i3lock -u -e -i /tmp/screen.png
+i3lock -e -i /tmp/screen.png
